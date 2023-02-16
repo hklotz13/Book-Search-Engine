4385b29520486ab1d989c93b545f51fa6fb80d18
Harper Klotz <harperklotz@Harpers-MacBook-Air.local>
2023-02-16 13:36:45 -0500

    updating app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,47 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+
+//graphql endpoint
+const httpLink = createHttpLink({uri: '/graphql'});
+
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+});
+
+//middleware pre-request to endpoint
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
+    <ApolloProvider client ={client}>
     <Router>
       <>
         <Navbar />
-        <Routes>
+        <Switch>
           <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
+            exact path='/' 
+            component={SearchBooks}/>
           <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
+            exact path='/saved' 
+            component={SavedBooks}/>
           <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
+            render={() => <h1 className='display-2'>Wrong page!</h1>}/>
+        </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
